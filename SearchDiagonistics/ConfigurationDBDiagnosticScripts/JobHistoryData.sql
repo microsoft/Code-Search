27677da7822abc199e93a9e3f51dfef00daf10ca
raggupta-ms <35366762+raggupta-ms@users.noreply.github.com>
2018-07-04 12:54:34 +0530

    Update JobHistoryData.sql

--- SearchDiagonistics/ConfigurationDBDiagnosticScripts/JobHistoryData.sql
+++ SearchDiagonistics/ConfigurationDBDiagnosticScripts/JobHistoryData.sql
@@ -16,6 +16,6 @@ SELECT [JobSource]
   (JobId = '02F271F3-0D40-4FA0-9328-C77EBCA59B6F' or JobId = '03CEE4B8-ECC1-4E57-95CE-FA430FE0DBFB' 
   or ResultMessage like '%completed with status%'
   or ResultMessage like '%back to Pending state with requeueDelay%'
-  or ResultMessage like '%Installed extension%')
-  or Result != 0
+  or ResultMessage like '%Installed extension%'
+  or Result != 0)
   order by StartTime desc
