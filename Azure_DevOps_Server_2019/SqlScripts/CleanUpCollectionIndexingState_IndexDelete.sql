2eed2ed2a81555575df0e8e27d161c49b750546b
Tapas Sahoo <tapass@microsoft.com>
2018-11-21 13:03:24 +0530

    Script corrections for obsolete and incorrect table cleanups

--- Azure_DevOps_Server_2019/SqlScripts/CleanUpCollectionIndexingState_IndexDelete.sql
+++ Azure_DevOps_Server_2019/SqlScripts/CleanUpCollectionIndexingState_IndexDelete.sql
@@ -1,8 +1,11 @@
 /**
 This script cleans up all the tables which has the state of indexing for all the indexing units of the Collection. 
 **/
-DELETE FROM [Search].[tbl_IndexingUnit] WHERE PartitionId > 0
-DELETE FROM [Search].[tbl_IndexingUnitChangeEvent] WHERE PartitionId > 0
-DELETE FROM [Search].[tbl_JobYield] WHERE PartitionId > 0
-DELETE FROM [Search].[tbl_TreeStore] WHERE PartitionId > 0
-DELETE FROM [Search].[tbl_ResourceLockTable] WHERE PartitionId > 0
+TRUNCATE TABLE [Search].[tbl_ClassificationNode]
+TRUNCATE TABLE [Search].[tbl_DisabledFiles]
+TRUNCATE TABLE [Search].[tbl_IndexingUnit]
+TRUNCATE TABLE [Search].[tbl_IndexingUnitChangeEvent]
+TRUNCATE TABLE [Search].[tbl_IndexingUnitWikis]
+TRUNCATE TABLE [Search].[tbl_ItemLevelFailures]
+TRUNCATE TABLE [Search].[tbl_JobYield]
+TRUNCATE TABLE [Search].[tbl_ResourceLockTable]
